03e3b6e9689fbf226f9fb4b007e1604a9cfac6a2
Ehsan Ziya <ehsan.ziya@gmail.com>
2015-05-04 18:07:16 +0300

    Task: Add tempo to constructor

--- build/wa-metro.js
+++ build/wa-metro.js
@@ -8,17 +8,17 @@ var cb = function (time, step) {
   osc.start(time);
   osc.stop(time + 0.1);
 };
-var metro = new Metro(context, 8, cb);
+var metro = new Metro(context, 120, 8, cb);
 metro.start();
 },{"./lib/wa-metro.js":2}],2:[function(require,module,exports){
 var interval = 50;
-var tempo = 120;
 var workerFile = window.URL.createObjectURL(new Blob(['(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module \'"+o+"\'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){\nvar interval = 25;\nself.onmessage = function (event) {\n  if (event.data === \'interval\') {\n    interval = event.data.interval;\n  }\n  if (event.data === \'start\') {\n    console.log(\'worker starting\');\n    setInterval(function () {\n      postMessage(\'tick\');\n    }, interval);\n  }\n};\n},{}]},{},[1])'],{type:"text/javascript"}));
 
-function Metro(context, resolution, cb) {
+function Metro(context, tempo, resolution, cb) {
   this.context = context;
   this.cb = cb;
   this.resolution = resolution;
+  this.tempo = tempo;
 
   this._worker = new Worker(workerFile);
   this._index = 1;
@@ -53,7 +53,7 @@ Metro.prototype._next = function _next() {
   if (this.index > this.resolution) {
     this.index = 1;
   }
-  this._next_note_time += ((60.0 / tempo) * 2) / this.resolution;
+  this._next_note_time += ((60.0 / this.tempo) * 2) / this.resolution;
 };
 
 module.exports = Metro;
