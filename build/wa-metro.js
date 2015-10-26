bfed1f67f5fbd9af5b47433eede6a85998e3f9b5
Ehsan Ziya <ziyaeddiny@gmail.com>
2015-10-26 11:20:32 +0000

    Task: Add proper standalone js support

--- build/wa-metro.js
+++ build/wa-metro.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.WAMetro = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = require('./lib/wa-metro');
 },{"./lib/wa-metro":2}],2:[function(require,module,exports){
 var work = require('webworkify');
@@ -23,7 +23,7 @@ function Metro(context, callback) {
 
   this._worker = work(require('./worker.js'));
 
-  this._worker.onmessage = function (event) {
+  this._worker.onmessage = function(event) {
     if (event.data === 'tick') {
       self._scheduler();
     }
@@ -34,7 +34,7 @@ function Metro(context, callback) {
   });
 }
 
-Metro.prototype.start = function (callback) {
+Metro.prototype.start = function(callback) {
   if (this._is_running) {
     console.log('already started');
     return;
@@ -43,12 +43,12 @@ Metro.prototype.start = function (callback) {
   this._worker.postMessage('start');
 };
 
-Metro.prototype.pause = function () {
+Metro.prototype.pause = function() {
   this._is_running = false;
   this._worker.postMessage('stop');
 };
 
-Metro.prototype.stop = function () {
+Metro.prototype.stop = function() {
   this._first = true;
   this._is_running = false;
   this._worker.postMessage('stop');
@@ -79,7 +79,7 @@ Metro.prototype._next = function _next() {
 };
 
 module.exports = Metro;
-window.Metro = Metro;
+
 },{"./worker.js":3,"webworkify":4}],3:[function(require,module,exports){
 module.exports = function (self) {
   var interval = 25;
@@ -157,4 +157,5 @@ module.exports = function (fn) {
     ));
 };
 
-},{}]},{},[1]);
+},{}]},{},[1])(1)
+});
